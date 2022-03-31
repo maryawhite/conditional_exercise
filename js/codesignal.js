280b3008c7dde815322503d0629ca0d659bb0e2b
Mary White <mary.a.white28@gmail.com>
2022-03-30 22:18:00 -0400

    added examples of javascript Array.map() method

--- js/codesignal.js
+++ js/codesignal.js
@@ -36,7 +36,10 @@ function order(words){
     if(words.length === 0) {
         return "";
     }
-    let parsed = parseInt(words, 10);
+    //split the words at the space
+    var splitWords = words.split(" ");
+    console.log("splitWords= " + splitWords);
+    var parsed = parseInt(splitWords, 10);
     console.log(parsed);
 
 }
@@ -50,3 +53,84 @@ function rowSumOddNumbers(n) {
     return n*n*n;
 }
 
+//map--the return value of the callback will be the new value of the element
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// var incremented = numbers.map(function(n){
+//     return n + 1;
+// });
+//
+// console.log(incremented);
+
+//written as an arrow function:
+const incremented = numbers.map(n => n + 1);
+
+console.log(incremented);
+
+//return a new array with the square root of all elements
+const numArr = [4, 9, 16, 25];
+const sqrootArr = numArr.map(Math.sqrt);
+console.log("The Array.map() method creates a new array from the results of calling a function for every element:\n " + sqrootArr);
+
+//use map to make an array of numbers that are doubles of the first array
+function doubleNumbers(arr){
+    const doubled = arr.map(n => n * 2);
+    return doubled;
+}
+console.log(doubleNumbers([2, 5, 100]));    //4, 10, 200
+
+function doubledNumbers2(arr) {
+    const doubled2 = arr.map(function(n){
+        return n * 2;
+    });
+    return doubled2;
+}
+
+console.log(doubledNumbers2([2, 5, 100]));     ///4, 10, 200
+
+//using map, take an array of numbers and make them strings
+function stringItUp(arr) {
+    const stringed = arr.map(n => n.toString());
+    return stringed;
+}
+
+console.log(stringItUp([3, 6, 101]));  ///["3", "6", "101"]
+
+function stringItUpToo(arr) {
+    const stringed2 = arr.map(function(n){
+        return n.toString();
+    });
+    return stringed2;
+}
+
+console.log(stringItUpToo([3, 6, 101])); ///["3", "6", "101"]
+
+//given a javascript object of names and ages, return an array of a string of names
+function namesOnly(arr){
+    const nameOnly = arr.map(n => n.name);
+    return nameOnly;
+}
+
+console.log(namesOnly([
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+]));
+// ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
